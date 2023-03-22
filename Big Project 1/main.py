9e110b0c43ab2ee8518311c3b846d6501198da55
EricKugel <erickugel713@gmail.com>
2023-03-22 09:36:51 -0500

    POstFix

--- Big Project 1/main.py
+++ Big Project 1/main.py
@@ -1,130 +1,109 @@
-# Interpreter for a language
-from functions import functions, operators, Object
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+alphaBET = alphabet + alphabet.upper()
+variables = alphaBET + "_"
+operators = ["+", "-", "*", "/", "//", "+=", "-=", "*=", "^" "/=", "//=", "%=", "%", "&", "!", "|", "<", "<=", ">", ">=", "==", "!="]
+precedence = {"^": 10,"~": 9.5,"!": 9.5,"*": 9,"/": 9,"//": 9,"%": 9,"+": 8,"-": 8,"<": 7,"<=": 7,">": 7,">=": 7,"==": 7,"!=": 7,"&": 6,"|": 5,"+=": 4,"-=": 4,"*=": 4,"/=": 4,"//=": 4,"%=": 4}
+numbers = "0.123456789"
 
-VALID_NUMBERS = "1234567890."
-VALID_OPS = "^*/+-%&|"
-ORDER_OF_OPS = ["^", "*/%", "+-", "&", "|"]
+import time
+import functions
 
-variables = {"true": True, "false": False}
+def is_operator(data, i):
+    return any([i + len(op) < len(data) and data[i:i+len(op)] == op for op in operators])
 
-def get_chunk_at(i, text):
-    open = text[i]
-    close = {
-        '(': ')',
-        '[': ']',
-        '"': '"'
-    }[open]
-    
-    j = i + 1
-    counter = 1
-    while counter != 0 and j < len(text):
-        if text[j] == open:
-            counter += 1
-            if open == '"' and not text[j - 1] == "\\":
-                return eval(text[i:j+1])
-        elif text[j] == ")":
-            counter -= 1
-        j += 1
-    return text[i:j]
+def find_operator(data, i):
+    return max([op for op in operators if i + len(op) < len(data) and data[i:i+len(op)] == op], key = lambda op : len(op))
 
-def clean(text):
-    new_text = ""
+def tokenize(data):
+    tokens = []
     i = 0
-    while i < len(text):
-        character = text[i]
-        if character in VALID_NUMBERS or character in VALID_OPS or character in "()":
-            new_text += character
-        elif is_function(i, text):
-            func = get_function(i, text)
-            new_text += func
-            i += len(func) - 1
-        elif is_variable(i, text):
-            var = get_variable(i, text)
-            new_text += var
-            i += len(var) - 1
-        i += 1
-    return new_text
-
-def is_function(i, text):
-    return any([text[i:].startswith(func) for func in functions.keys()])
-
-def is_variable(i, text):
-    return any([text[i:].startswith(var) for var in variables.keys()])
-    
-def get_function(i, text):
-    return max([func for func in functions.keys() if text[i:].startswith(func)], key = lambda func : len(func))
-
-def get_variable(i, text):
-    return max([var for var in variables.keys() if text[i:].startswith(var)], key = lambda var : len(var))
-
-def parse(text):
-    text = clean(text)
-    parsed = []
-    # is_op = False
-    i = 0
-    while i < len(text):
-        character = text[i]
-        if character in '(["':
-            chunk = get_chunk_at(i, text)
-            i += len(chunk)
-            if character != '"':
-                parsed.append(chunk[0])
-                parsed.append(parse(chunk[1:-1]))
-                parsed.append(chunk[-1])
+    token = ""
+    while i < len(data):
+        char = data[i]
+        if char in "(),:":
+            if len(token) > 0:
+                tokens.append(token)
+            token = ""
+            tokens.append(char)
+            i += 1
+        elif char in " \t":
+            if len(token) > 0:
+                tokens.append(token)
+            token = ""
+            i += 1
+        elif char == '"':
+            if len(token) > 0:
+                tokens.append(token)
+            token = ""
+            token += char
+            i += 1
+            while i < len(data) and not(data[i] == '"' and data[i - 1] != "\\"):
+                token += data[i]
+                i += 1
+            token += char
+            i += 1
+            tokens.append(token)
+            token = ""
+        elif is_operator(data, i):
+            if len(token) > 0:
+                tokens.append(token)
+            op = find_operator(data, i)
+            if op == "-" and not (tokens[-1] == ")" or all([letter in variables for letter in tokens[-1]]) or all([letter in numbers for letter in tokens[-1]])):
+                token = "~"
             else:
-                parsed.append(parse(chunk[1:-1]))
-            # is_op = True
+                token = op
+            i += len(op)
+        elif char in variables:
+            if len(token) > 0:
+                tokens.append(token)
+            token = ""
+            while i < len(data) and data[i] in variables:
+                token += data[i]
+                i += 1
+        elif char in numbers:
+            if len(token) > 0:
+                tokens.append(token)
+            token = ""
+            while i < len(data) and data[i] in numbers:
+                token += data[i]
+                i += 1
         else:
-            if is_function(i, text):
-                # reduce function to a constant
-                func = get_function(i, text)
-                func_input = get_chunk_at(text.index("(", i + 1), text)
-                i += len(func_input) + len(func)
-                func_input = evaluate(parse(func_input[1:-1]))
-                parsed.append(functions[func](func_input))
-            elif is_variable(i, text):
-                var = get_variable(i, text)
-                i += len(var)
-                parsed.append(variables[var])
-            else:
-                token = ""
-                while character in VALID_NUMBERS and i < len(text):
-                    token += character
-                    i += 1
-                    try:
-                        character = text[i]
-                    except Exception:
-                        character = ""
-                if character in VALID_OPS:
-                    parsed.append(character)
-                parsed.append(float(token))
-            # is_op = True
-    return parsed
+            i += 1
+    return tokens
 
-def evaluate(parsed):
-    for item in parsed:
-        if isinstance(item, list):
-            item = evaluate(item)
-    for current_op in ORDER_OF_OPS:
-        i = 0
-        while i < len(parsed) - 2:
-            op = parsed[i + 1]
-            if op in current_op:
-                result = operators(op)(parsed[i], parsed[i + 2])
-                parsed[i + 1] = result
-                parsed.pop(i + 2)
-                parsed.pop(i)
-                i -= 2
-            i += 2
-    return parsed[0]
+def postfix(tokens):
+    output = []
+    stack = []
+    while tokens:
+        token = tokens.pop(0)
+        if all([letter in numbers for letter in token]):
+            output.append(token)
+        elif token[0] == '"':
+            output.append(token)
+        elif all([letter in variables for letter in token]) and not tokens[0] == "(":
+            output.append(token)
+        elif all([letter in variables for letter in token]):
+            stack.append(token)
+        elif token in operators:
+            while stack[-1] in operators and precedence[stack[-1]] >= precedence[token]:
+                output.append(stack.pop())
+            stack.append(token)
+        elif token == "(":
+            stack.append(token)
+        elif token == ")":
+            while stack[-1] != "(":
+                output.append(stack.pop())
+            stack.pop()
+            if stack and all([letter in variables for letter in stack[-1]]):
+                output.append(stack.pop())
+    while stack:
+        output.append(stack.pop())
+    return output
 
-data = []
-with open("Big Project 1/input.esar", "r") as file:
-    data = [line.strip() for line in file.readlines()]
+def evaluate(tokens):
+    stack = []
+    for token in tokens:
+        if 
 
-for line in data:
-    if "=" in line:
-        assignment = line.split("=")
-        variables[assignment[0].strip()] = evaluate(parse(assignment[1]))
-    else:
-        evaluate(parse(line))
+
+print(postfix(tokenize("sin ( max ( 2, 3 ) / 3 * pi )")))
