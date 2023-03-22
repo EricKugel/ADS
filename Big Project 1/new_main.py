6df8a0f930bcaa560aae60d9056c3ed83014e114
EricKugel <erickugel713@gmail.com>
2023-03-21 23:39:11 -0500

    Gotta commit

--- Big Project 1/new_main.py
+++ Big Project 1/new_main.py
@@ -1,7 +1,8 @@
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 alphaBET = alphabet + alphabet.upper()
 variables = alphaBET + "_"
-operators = ["+", "-", "*", "/", "//", "+=", "-=", "*=", "/=", "//=", "%=", "%", "&", "!", "|", "<", "<=", ">", ">=", "==", "!="]
+operators = ["+", "-", "*", "/", "//", "+=", "-=", "*=", "^" "/=", "//=", "%=", "%", "&", "!", "|", "<", "<=", ">", ">=", "==", "!="]
+precedence = {"^": 10,"~": 9.5,"!": 9.5,"*": 9,"/": 9,"//": 9,"%": 9,"+": 8,"-": 8,"<": 7,"<=": 7,">": 7,">=": 7,"==": 7,"!=": 7,"&": 6,"|": 5,"+=": 4,"-=": 4,"*=": 4,"/=": 4,"//=": 4,"%=": 4}
 numbers = "0.123456789"
 
 import time
@@ -18,12 +19,30 @@ def tokenize(data):
     token = ""
     while i < len(data):
         char = data[i]
-        if char in "([\{\}]),:;":
+        if char in "(),:;":
             if len(token) > 0:
                 tokens.append(token)
             token = ""
             tokens.append(char)
             i += 1
+        elif char in "[\{":
+            if len(token) > 0:
+                tokens.append(token)
+            token = ""
+            open = char
+            close = {"[": "]", "{": "}"}[open]
+            block = "" + char
+            counter = 1
+            i += 1
+            while i < len(data) and counter > 0:
+                char = data[i]
+                block += str(char)
+                if char == close:
+                    counter -= 1
+                elif char == open:
+                    counter += 1
+                i += 1 
+            tokens.append([open] + tokenize(block[1:-1]) + [close])
         elif char in " \t\n":
             if len(token) > 0:
                 tokens.append(token)
@@ -45,9 +64,11 @@ def tokenize(data):
         elif is_operator(data, i):
             if len(token) > 0:
                 tokens.append(token)
-            token = ""
             op = find_operator(data, i)
-            token += op
+            if op == "-" and not (tokens[-1] == ")" or all([letter in variables for letter in tokens[-1]]) or all([letter in numbers for letter in tokens[-1]])):
+                token = "~"
+            else:
+                token = op
             i += len(op)
         elif char in variables:
             if len(token) > 0:
@@ -63,6 +84,16 @@ def tokenize(data):
             while i < len(data) and data[i] in numbers:
                 token += data[i]
                 i += 1
+        else:
+            i += 1
     return tokens
 
-print("\n".join(tokenize("a+=b-(sin(23/pi))+\"hello world\"")))
+def postfix(tokens):
+    output = []
+    stack = []
+    while tokens:
+        token = tokens.pop(0)
+        if all([letter in numbers for letter in token]):
+            output.append(token)
+        elif token == "{":
+            
